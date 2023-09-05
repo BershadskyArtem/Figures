28ecc31c96825163e4939b613b9fe6c8287e54be
Bershadsky Artem <bershadskyartem@gmail.com>
2023-09-05 22:34:27 +0300

    Added implementation for basic figures

---
+++ Figures.Core/Logic/Implementations/CircleFigure.cs
@@ -0,0 +1,20 @@
+﻿using Figures.Core.Logic.Abstractions;
+
+namespace Figures.Core.Logic.Implementations;
+
+public class CircleFigure : IFigure
+{
+    private readonly double _radius;
+
+    public CircleFigure(double radius)
+    {
+        _radius = radius;
+    }
+    
+    public string Name { get; } = nameof(CircleFigure);
+    
+    public double GetArea()
+    {
+        return 2 * Math.PI * _radius;
+    }
+}
---
+++ Figures.Core/Logic/Implementations/TriangleFigure.cs
@@ -0,0 +1,25 @@
+﻿using Figures.Core.Logic.Abstractions;
+
+namespace Figures.Core.Logic.Implementations;
+
+public class TriangleFigure : IFigure
+{
+    private readonly double _a;
+    private readonly double _b;
+    private readonly double _c;
+
+    public TriangleFigure(double a, double b, double c)
+    {
+        _a = a;
+        _b = b;
+        _c = c;
+    }
+    
+    public string Name { get; } = nameof(TriangleFigure);
+    
+    public double GetArea()
+    {
+        double halfPerimeter = (_a + _b + _c) / 2.0;
+        return Math.Sqrt(halfPerimeter * (halfPerimeter * _a) * (halfPerimeter * _b) * (halfPerimeter * _c));
+    }
+}
