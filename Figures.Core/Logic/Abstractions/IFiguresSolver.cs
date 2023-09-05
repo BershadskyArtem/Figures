60a8cab52f39bef6cf05f8d9389a7c9bf8c81669
Bershadsky Artem <bershadskyartem@gmail.com>
2023-09-05 22:29:41 +0300

    Added abstractions for figures and sovler.

---
+++ Figures.Core/Logic/Abstractions/IFiguresSolver.cs
@@ -0,0 +1,7 @@
+﻿namespace Figures.Core.Logic.Abstractions;
+
+public interface IFiguresSolver
+{
+    public double CalculateArea(IFigure figure);
+    public double CalculateArea(IEnumerable<IFigure> figures, string expectedType);
+}
